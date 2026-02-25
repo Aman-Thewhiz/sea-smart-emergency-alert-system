ba452378b820e0148b037b547550b1b5fd258bc3
Aman-Thewhiz <amanranju23@gmail.com>
2026-02-25 23:01:28 +0530

    Fix gunicorn_conf.py merge conflict

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,17 +1,19 @@
-<<<<<<< HEAD
+import multiprocessing
 import os
 
 bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
-workers = int(os.getenv("WEB_CONCURRENCY", "2"))
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "30"))
-accesslog = "-"
-errorlog = "-"
-=======
-import os
 
-bind = f"0.0.0.0:{os.getenv('PORT', '5000')}"
-workers = int(os.getenv("WEB_CONCURRENCY", "2"))
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "30"))
+workers = 1
+threads = 4
+
+timeout = 120
+graceful_timeout = 120
+
+keepalive = 5
+
+worker_class = "gthread"
+
+loglevel = "info"
+
 accesslog = "-"
 errorlog = "-"
->>>>>>> 9ff4e1b8de1c80ab4df041ad2aa60eacb793586e
